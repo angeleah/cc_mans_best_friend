ff2485a4255f367353d73b4c4c0566a78f629881
Angeleah Daidone <angeleah@gmail.com>
2013-10-26 20:44:51 -0500

    Initial Commit

---
+++ Man's Best Friend/ADDog.h
@@ -0,0 +1,13 @@
+//
+//  ADDog.h
+//  Man's Best Friend
+//
+//  Created by Angeleah Daidone on 10/26/13.
+//  Copyright (c) 2013 Angeleah Daidone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADDog : NSObject
+
+@end
---
+++ Man's Best Friend/ADDog.m
@@ -0,0 +1,13 @@
+//
+//  ADDog.m
+//  Man's Best Friend
+//
+//  Created by Angeleah Daidone on 10/26/13.
+//  Copyright (c) 2013 Angeleah Daidone. All rights reserved.
+//
+
+#import "ADDog.h"
+
+@implementation ADDog
+
+@end
